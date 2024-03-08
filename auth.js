6f3a9a5a1dcf06229277c5d31be6af84f601bd2d
anthonypetersen <tonypetersen@hotmail.com>
2024-03-08 13:32:47 -0600

    updates

--- auth.js
+++ auth.js
@@ -1,7 +1,46 @@
-const { setHeaders } = require('./shared');
+const { setHeaders, fetchSecrets } = require('./shared');
+const { Octokit } = require('octokit');
 
 const ghauth = async (req, res) => {
+    setHeaders(res);
+    
+    if(req.method === 'OPTIONS') return res.status(200).json({code: 200});
 
+    const api = req.query.api;
+
+    if (api === 'accessToken') {
+        try {
+
+            console.log(`POST request received for ${api} API`);
+
+            const secrets = await fetchSecrets();
+
+            const code = req.body.code;
+            console.log(code);
+            const REDIRECT_URI = 'http://localhost:5000/';
+
+            const tokenResponse = await fetch('https://github.com/login/oauth/access_token', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify({
+                    client_id: secrets.client_id,
+                    client_secret: secrets.client_secret,
+                    code: code,
+                    redirect_uri: REDIRECT_URI
+                })
+            });
+    
+            const response = await tokenResponse.json();
+            res.status(200).json(response);
+
+        } catch (error) {
+            console.error('Error:', error);
+            res.status(500).json({error: 'Internal Server Error'});
+        }
+    }
     
 }
 
--- shared.js
+++ shared.js
@@ -4,6 +4,24 @@ const setHeaders = (res) => {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 }
 
+const fetchSecrets = async () => {
+    const secrets = {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET
+    };
+
+    const client = new SecretManagerServiceClient();
+    let fetchedSecrets = {};
+
+    for (const [key, value] of Object.entries(secretsToFetch)) {
+        const [version] = await client.accessSecretVersion({ name: value });
+        fetchedSecrets[key] = version.payload.data.toString();
+    }
+    
+    return fetchedSecrets;
+}
+
 module.exports = {
-    setHeaders
+    setHeaders,
+    fetchSecrets
 }
