c0fbfad4d2be6c516d1200a38cf5784b5a7864da
anthonypetersen <tonypetersen@hotmail.com>
2024-09-16 11:04:21 -0500

    modify endpoint

--- auth.js
+++ auth.js
@@ -74,12 +74,7 @@ const ghauth = async (req, res) => {
                 auth: token
             });
 
-            const { owner, repo, path, message, content, sha } = req.body;
-            console.log(`Owner: ${owner}`);
-            console.log(`Repo: ${repo}`);
-            console.log(`Path: ${path}`);
-            console.log(`Message: ${message}`);
-            console.log(`Content: ${content}`);
+            const { owner, repo, path, message, content } = req.body;
 
             const response = await octokit.request(`PUT /repos/{owner}/{repo}/contents/{path}`, {
                 owner,
@@ -87,6 +82,37 @@ const ghauth = async (req, res) => {
                 path,
                 message,
                 content,
+                headers: {
+                  'X-GitHub-Api-Version': '2022-11-28'
+                }
+            });
+
+            res.status(200).json(response);
+        } catch (error) {
+            console.error('Error:', error);
+            res.status(500).json({error: 'Internal Server Error'});
+        }
+    }
+
+    if (api === 'updateFile') {
+        try {
+            if (req.method !== 'POST') return res.status(405).json({error: 'Method Not Allowed'});
+
+            const token = req.headers.authorization.replace('Bearer','').trim();
+
+            const octokit = new Octokit({
+                auth: token
+            });
+
+            const { owner, repo, path, message, content, sha } = req.body;
+
+            const response = await octokit.request(`PUT /repos/{owner}/{repo}/contents/{path}`, {
+                owner,
+                repo,
+                path,
+                message,
+                content,
+                sha,
                 headers: {
                   'X-GitHub-Api-Version': '2022-11-28'
                 }
@@ -110,9 +136,6 @@ const ghauth = async (req, res) => {
             });
 
             const { owner, repo, path } = req.query;
-            console.log(`Owner: ${owner}`);
-            console.log(`Repo: ${repo}`);
-            console.log(`Path: ${path}`);
 
             const response = await octokit.request(`GET /repos/{owner}/{repo}/contents/{path}`, {
                 owner,
@@ -129,7 +152,6 @@ const ghauth = async (req, res) => {
             res.status(500).json({error: 'Internal Server Error'});
         }
     }
-    
 }
 
 module.exports = {
