f127036aed1ce5dcb7720581501d12e1237cfa77
anthonypetersen <tonypetersen@hotmail.com>
2024-10-09 16:00:48 -0500

    update

--- auth.js
+++ auth.js
@@ -149,7 +149,7 @@ const ghauth = async (req, res) => {
 
             console.log(zipData);
 
-            res.status(200).json(response);
+            res.status(200).json({data: zipData});
         } catch (error) {
             console.error('Error:', error);
             res.status(500).json({error: 'Internal Server Error'});
